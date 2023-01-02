44a6d24e46c98f12900f875c23f1ffef77f7f036
Catherine Miguel <catmig1992@gmail.com>
2023-01-02 00:39:33 -0800

    StoreListings

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import './App.css';
+import "./App.css";
 import TopBar from "./components/TopBar";
 import Background from "./components/Background";
+import StoreListings from "./components/StoreListing";
 
 function App() {
   return (
     <div className="App">
       <TopBar />
       <Background />
+      <StoreListings />
     </div>
   );
 }
