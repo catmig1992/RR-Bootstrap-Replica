21142cf1a093ef3359c4ef4ab840765e0dc282c7
Catherine Miguel <catmig1992@gmail.com>
2023-01-02 00:49:37 -0800

    add image and column style

--- src/components/StoreListing.jsx
+++ src/components/StoreListing.jsx
@@ -5,8 +5,18 @@ import Col from "react-bootstrap/Col";
 
 export default function StoreListings() {
   return (
-    <div>
-      <h2>Browse stores in Houston</h2>
+    <div style={{ margin: "auto" }}>
+      <h2
+        style={{
+          fontSize: "36px",
+          lineHeight: "50px",
+          marginTop: "20px",
+          textAlign: "center",
+          fontWeight: "normal",
+        }}
+      >
+        Browse stores in Houston
+      </h2>
       <Container style={{ marginTop: "40px", textAlign: "center" }}>
         <Row
           style={{
@@ -17,9 +27,17 @@ export default function StoreListings() {
         >
           <Row>
             <Col xs=".5">
-              <Image src="https://d2d8wwwkmhfcva.cloudfront.net/96x96/d2lnr5mha7bycj.cloudfront.net/warehouse/logo/12/2774c343-1319-4720-a54f-0c46e93f956c.png" roundedCircle />
+              <Image
+                style={{
+                  height: "55px",
+                  width: "50px",
+                  border: "1px solid #E8E9EB",
+                }}
+                src="https://d2d8wwwkmhfcva.cloudfront.net/96x96/d2lnr5mha7bycj.cloudfront.net/warehouse/logo/12/2774c343-1319-4720-a54f-0c46e93f956c.png"
+                roundedCircle
+              />
             </Col>
-            <Col>
+            <Col style={{ textAlign: "left" }}>
               <h5 style={{ fontWeight: "bold" }}>ALDI</h5>
               <p>Delivery * Pickup</p>
             </Col>
@@ -28,11 +46,16 @@ export default function StoreListings() {
           <Row>
             <Col xs=".5">
               <Image
+                style={{
+                  height: "50px",
+                  width: "50px",
+                  border: "1px solid #E8E9EB",
+                }}
                 src="https://d2d8wwwkmhfcva.cloudfront.net/96x96/d2lnr5mha7bycj.cloudfront.net/warehouse/logo/279/0d43b5d2-9d07-4a30-a0ee-0e16d5922411.png"
                 roundedCircle
               />
             </Col>
-            <Col>
+            <Col style={{ textAlign: "left" }}>
               <h5 style={{ fontWeight: "bold" }}>Sprouts Farmers Market</h5>
               <p>Delivery * Pickup</p>
             </Col>
@@ -41,11 +64,16 @@ export default function StoreListings() {
           <Row>
             <Col xs=".5">
               <Image
+                style={{
+                  height: "50px",
+                  width: "50px",
+                  border: "1px solid #E8E9EB",
+                }}
                 src="https://d2d8wwwkmhfcva.cloudfront.net/96x96/d2lnr5mha7bycj.cloudfront.net/warehouse/logo/45/d10d0f4b-1fca-4b84-bc19-bbf492c040da.png"
                 roundedCircle
               />
             </Col>
-            <Col>
+            <Col style={{ textAlign: "left" }}>
               <h5 style={{ fontWeight: "bold" }}>Kroger</h5>
               <p>Delivery</p>
             </Col>
